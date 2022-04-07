b6351f05fa3618a5cbeb83ee01baab5f86fe548e
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-04-07 19:24:34 -0300

    commit inicial

---
+++ ler _jogojava/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        String jogo;
+
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Jogo:");
+        jogo = leitor.next();
+
+        System.out.println("jogo é " + jogo);
+
+    }
+}
