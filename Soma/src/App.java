c28d97325e9ac646cd69f31a65e0b2130222c590
Calculadora <aldo.santos.oliveira95@gmail.com>
2022-04-08 21:30:33 -0300

    commit inicial

---
+++ Soma/src/App.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int A = input.nextInt();
+        int B = input.nextInt();
+        int Soma = A + B;
+
+        System.out.println("Soma = " + Soma);
+
+        input.close();
+
+    }
+}
